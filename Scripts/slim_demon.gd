3c66beb47899685270f6a091b2bdf43034987e47
DecotDev <decot013@gmail.com>
2024-11-05 17:10:24 +0100

    PlayerMovement
    + Horizontal movment
    + Jump
    + Double Jump

--- Scripts/slim_demon.gd
+++ Scripts/slim_demon.gd
@@ -1,31 +1,17 @@
 extends CharacterBody2D
 
-enum { MOVE }
-var state = MOVE
-var buffered_jump = false
-var coyote_jump = false
-var max_speed = 300
-var acceleration = 200
+var gravity = 100
 var friction = 100
-var double_jump = 1
+var max_speed = 400
+var acceleration = 180
+var jump_force = -1500
+var double_jumps = 1
 
-# Player movement variables
-@export var speed = 300
-@export var jump_strength = 1800
-@export var jump_release_strength = 1800
-@export var gravity = 7000
-
-
-# This function will run every frame and handle the player's movement and jumping
 func _physics_process(delta: float) -> void:
 	var input = Vector2.ZERO
 	input.x = Input.get_axis("ui_left", "ui_right")
-	input.y = Input.get_axis("ui_up", "ui_down")
+	#input.y = Input.get_axis("ui_up", "ui_down")
 	
-	match state:
-		MOVE: move_state(input)
-
-func move_state(input):
 	apply_gravity()
 	
 	if not horizontal_move(input):
@@ -35,47 +21,27 @@ func move_state(input):
 		apply_acceleration(input.x)
 		%AnimatedSprite2D.play("Run")
 		%AnimatedSprite2D.flip_h = input.x < 0
-		move_and_slide()
-		
+	
+	input_jump()
 	if is_on_floor():
-		reset_double_jump()
+		double_jumps = 1
 	else:
-		%AnimatedSprite2D.play("Jumping")
-		
-	if can_jump():
-		input_jump()
-	else:
-		input_jump_release()
 		input_double_jump()
-		#buffer_jump()
-		#fast_fall()
 	
-func apply_gravity():
-	velocity.y += gravity
-	velocity.y = min(velocity.y, 290)
-func apply_friction():
-	velocity.x = move_toward(velocity.x, 1, friction)
-func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, max_speed * amount, acceleration)
-	
+	move_and_slide()
+
 func horizontal_move(input):
 	return input.x != 0
-	
+func apply_gravity():
+	velocity.y += gravity
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, friction)
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, max_speed * amount, acceleration)
 func input_jump():
-	if Input.is_action_just_pressed("ui_up") or buffered_jump == true:
-		#SoundPlayer.play_sound(SoundPlayer.JUMP)
-		velocity.y = jump_strength
-		buffered_jump = false
-func input_jump_release():
-	if Input.is_action_just_released("ui_up") and velocity.y < jump_release_strength:
-		velocity.y = jump_release_strength
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_force
 func input_double_jump():
-	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
-		#SoundPlayer.play_sound(SoundPlayer.JUMP)
-		velocity.y = jump_strength
-		double_jump -= 1
-
-func reset_double_jump():
-	double_jump = 1
-func can_jump():
-	return is_on_floor() or coyote_jump
+	if Input.is_action_just_pressed("ui_up") and double_jumps > 0:
+		velocity.y = jump_force
+		double_jumps -= 1
