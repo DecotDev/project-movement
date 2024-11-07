ebbed8e8694ce40990f73818577dc7d7fdd31e43
DecotDev <decot013@gmail.com>
2024-11-07 16:46:16 +0100

    Added Camera2D
    New
    - Camera2D
    - Fixed blurry camera position smoothing

--- Scripts/slim_demon.gd
+++ Scripts/slim_demon.gd
@@ -60,7 +60,7 @@ func apply_acceleration(amount):
 	velocity.x = move_toward(velocity.x, max_speed * amount, acceleration)
 
 func input_jump():
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("Up")) and is_on_floor():
 		velocity.y = jump_force
 
 func input_double_jump():
