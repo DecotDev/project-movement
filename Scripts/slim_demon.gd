60d87809e037a04aeb263192570c69abc1ef318b
DecotDev <decot013@gmail.com>
2024-11-19 17:17:40 +0100

    Wall Slide
    Now you can wall slide on walls, and use a double jump to wall jump.
    - New bug, wall slide animation blinks

--- Scripts/slim_demon.gd
+++ Scripts/slim_demon.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
-var gravity = 100
+var gravity = 82
+var max_gravity = 1300
 var friction = 100
 var max_speed = 550
 var acceleration = 180
-var jump_force = -1700
-var double_jumps = 1
+var jump_force = -1300
+var double_jumps = 10
 var is_crouching = false # New flag for crouch state
+var is_sliding = false
 
 func _physics_process(delta: float) -> void:
 	var input = Vector2.ZERO
@@ -26,7 +28,10 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("ui_down") or Input.is_action_pressed("Down"):
 		input.y = 1
 
-	apply_gravity()
+	if can_wall_slide():
+		wall_slide()
+	else:
+		apply_gravity()
 
 	# Handle crouch input and prevent other animations from playing if crouching
 	if can_crouch() and (Input.is_action_pressed("ui_down") or Input.is_action_pressed("Down")):
@@ -36,11 +41,11 @@ func _physics_process(delta: float) -> void:
 		
 		if not horizontal_move(input):
 			apply_friction()
-			if !is_crouching:
+			if !is_crouching and !is_sliding:
 				%AnimatedSprite2D.play("Idle")
 		else:
 			apply_acceleration(input.x)
-			if !is_crouching:
+			if !is_crouching and !is_sliding:
 				%AnimatedSprite2D.play("Run")
 			%AnimatedSprite2D.flip_h = input.x < 0
 		
@@ -48,15 +53,16 @@ func _physics_process(delta: float) -> void:
 			input_jump()  # Check for jump input
 
 		if is_on_floor():
-			double_jumps = 1  # Reset double jumps on the floor
+			double_jumps = 10  # Reset double jumps on the floor
 		else:
 			input_double_jump()  # Handle double jump input
 			if velocity.y < 200:
-				if !is_crouching:
+				if !is_crouching and !is_sliding:
 					%AnimatedSprite2D.play("Falling")
 			else: 
-				if !is_crouching:
+				if !is_crouching and !is_sliding:
 					%AnimatedSprite2D.play("Jumping")
+
 	
 	move_and_slide()
 
@@ -64,7 +70,9 @@ func horizontal_move(input):
 	return input.x != 0
 
 func apply_gravity():
+	is_sliding = false
 	velocity.y += gravity
+	velocity.y = min(velocity.y, max_gravity)
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, friction)
@@ -82,12 +90,21 @@ func input_double_jump():
 	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("Up")) and double_jumps > 0:
 		velocity.y = jump_force
 		double_jumps -= 1
+		
+func wall_slide():
+	is_sliding = true
+	velocity.y += gravity
+	velocity.y = min(velocity.y, 200)
+	%AnimatedSprite2D.play("WallSliding")
 
 func can_jump():
 	return is_on_floor()
 
 func can_crouch():
 	return is_on_floor()
+	
+func can_wall_slide():
+	return is_on_wall() and !is_on_floor() and (velocity.y > 200)
 
 func input_crouch():
 	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("Down"):
