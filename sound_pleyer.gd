f804ee0b3588dfce11cac53ee9441367ca8a487a
DecotDev <decot013@gmail.com>
2025-01-01 12:27:51 +0100

    SoundPlayer + Coin
    Added the functional sound player capable of playing the coin sound

--- sound_pleyer.gd
+++ sound_pleyer.gd
@@ -1,10 +1,13 @@
 extends Node
 
+const pick_coin = preload("res://Assets/Sounds/Coin/coin0.wav") 
+
 @onready
 var audioPlayers: = $AudioPlayers
 
-func play_sound():
+func play_sound(sound):
 	for audioStreamPlayer in audioPlayers.get_children():
 		if not audioStreamPlayer.playing:
-			audioStreamPlayer
-		pass
+			audioStreamPlayer.stream = sound
+			audioStreamPlayer.play()
+			break
