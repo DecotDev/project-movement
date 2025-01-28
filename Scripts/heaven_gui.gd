7d55a46bfa4efceb61614e44f125bfd0f16c33de
DecotDev <decot013@gmail.com>
2025-01-28 18:20:37 +0100

    Add spike method call

--- Scripts/heaven_gui.gd
+++ Scripts/heaven_gui.gd
@@ -6,6 +6,8 @@ var heart_empty: = preload("res://Assets/Heaven/GUI/godot_icon_black.png")
 func update_health_icon() -> void:
 	for i in get_child_count():
 		if Global.player_health > i:
-			get_child(i).texture = heart_full
+			print("vida")
+			#get_child(i).texture = heart_full
 		else:
-			get_child(i).texture = heart_empty
+			print("-1 vida")
+			#get_child(i).texture = heart_empty
--- Scripts/spike.gd
+++ Scripts/spike.gd
@@ -1,10 +1,13 @@
 extends Sprite2D
 #var heaven_gui: = get_tree().root.get_node("/root/HeavenGuid")
 #heaven_gui: = get_tree().root.get_node("./HeavenGUI")
-
+var gui: Node = null
+func _ready() -> void:
+	gui = get_tree().get_root().find_child("HeavenGUI", true, false)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	Global.player_health -= 1
+	gui.update_health_icon()
 	#heaven_gui.update_health_icon()
 	#heaven_gui.update_health_icon()
 	#get_tree().reload_current_scene()
