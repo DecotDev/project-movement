85456d3e39dbbd28c5b7a367b55e7abebe9257e6
DecotDev <decot013@gmail.com>
2025-01-30 23:25:04 +0100

    Gun fixed, ammo system
    What's new?
    - Fixed the gun
    - Bullets fixed
    - Added change the hand holding the gun based on mouse position
    - Added ammo system
    - Reload

--- Scripts/demon.gd
+++ Scripts/demon.gd
@@ -1,17 +1,44 @@
 extends CharacterBody2D
 
-
 class_name Character
+
+@onready
+var mouse_label: = %MouseLabel
+@onready
+var sprite: = %Sprite2D
+@onready
+var gun: = %Gun
+
+var mouse_pos: Vector2
+var gun_moved_left: bool = false
+var gun_moved_right: bool = false
+
 var input_direction: Vector2
 var movement_speed: int = 40
 
-
 func _physics_process(delta: float) -> void:
+	mouse_pos = get_global_mouse_position()
 	input_direction = Input.get_vector("Left","Right","Up","Down")
 	#input_direction = input_direction.normalized()
 	velocity = input_direction * 550
 	move_and_slide()
 	
-	#look_at(get_global_mouse_position())
+	if mouse_pos.x < position.x:
+		sprite.flip_h = false
+		if !gun_moved_left:
 
- 
+			gun.position.x = -16
+			gun_moved_left = true
+			gun_moved_right = false
+	else:
+		sprite.flip_h = true
+		if !gun_moved_right:
+			gun.position.x = + 16
+			gun_moved_left = false
+			gun_moved_right = true
+
+	if Input.is_action_just_pressed("Test"):
+		gun.position = Vector2(0,0)
+	
+	mouse_label.text = ("Char X: " + str(position.x) + "\nChar Y: " + str(position.y) + "\nMaus X: " + str(get_global_mouse_position()))
+	#look_at(get_global_mouse_position())
--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,13 +1,31 @@
 extends Area2D
 
+@onready
+var reload_timer: Node = %ReloadTimer
+
+var magazine_size: int = 8
+var ammo: int = 8
+var reloading: bool = false
+
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("Shoot"):
-		shoot()
+	if Input.is_action_just_pressed("Shoot") and !reloading:
+		if ammo > 0: shoot()
+	if Input.is_action_just_pressed("Reload") and ammo != magazine_size and !reloading:
+		reload()
 
 func shoot() -> void:
+	ammo -= 1
 	const BULLET = preload("res://Scences/bullet.tscn")
 	var new_bullet: = BULLET.instantiate()
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
+
+func reload() -> void:
+	reloading = true
+	reload_timer.start()
+
+func _on_reload_timer_timeout() -> void:
+	ammo = magazine_size
+	reloading = false
