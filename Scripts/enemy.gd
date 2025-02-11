0786651e29205f4472f2c1ed8a54ec98fc02dc90
DecotDev <decot013@gmail.com>
2025-02-11 18:27:06 +0100

    Enemy update
    What's new?
    - New enemy that moves toward de player
    
    TO DO
    - Kill enemy
    - Be damaged by the enemy

---
+++ Scripts/enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready
+var demon: CharacterBody2D
+
+var direction: Vector2
+var rng: = RandomNumberGenerator.new()
+var dir_countdown: int = 0
+
+#func _ready() -> void:
+	#demon = get_node("/root/HellMain/Demon")
+func _ready() -> void:
+	demon = get_tree().get_root().find_child("Demon", true, false)
+
+func _process(delta: float) -> void:
+	#direction = global_position.direction_to(demon.global_position)
+	if dir_countdown <= 0:
+		direction = global_position.direction_to(demon.global_position)
+		changeDirection()
+		dir_countdown = rng.randf_range(20,60)
+	dir_countdown -= 1
+	velocity = direction * 180.0 
+	move_and_slide()
+
+func changeDirection() -> void:
+	direction.x += rng.randf_range(-0.6,0.6)
+	direction.y += rng.randf_range(-0.6,0.6)
