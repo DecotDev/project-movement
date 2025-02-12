7ef7ac0f3ccef1edf8e97aeb7f1dc1e3a11cdd9b
DecotDev <decot013@gmail.com>
2025-02-12 20:20:13 +0100

    Enemies take damage
    What's new?
    - Now you can kill the enemies around the player

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -18,3 +18,9 @@ func _physics_process(delta: float) -> void:
 	#queue_free()
 	#if body.has_method("take_damage"):
 		#body.take_damage()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready
 var demon: CharacterBody2D
 
+var health: int = 3
 var direction: Vector2
 var rng: = RandomNumberGenerator.new()
 var dir_countdown: int = 0
@@ -11,6 +12,7 @@ var dir_countdown: int = 0
 	#demon = get_node("/root/HellMain/Demon")
 func _ready() -> void:
 	demon = get_tree().get_root().find_child("Demon", true, false)
+	%AnimationPlayer.play("Idle")
 
 func _process(delta: float) -> void:
 	#direction = global_position.direction_to(demon.global_position)
@@ -25,3 +27,11 @@ func _process(delta: float) -> void:
 func changeDirection() -> void:
 	direction.x += rng.randf_range(-0.6,0.6)
 	direction.y += rng.randf_range(-0.6,0.6)
+
+func take_damage() -> void:
+	%AnimationPlayer.play("Hurt")
+	health -= 1
+	if health <= 0:
+		queue_free()
+	
+	
