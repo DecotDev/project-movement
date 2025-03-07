772ea8cc4d16f282eeba21985ff9d0d819790399
DecotDev <decot013@gmail.com>
2025-03-07 19:38:14 +0100

    Shells falloff adjust + debug
    What's new?
    - Shells ejection animation
    - Debug Menu addon

--- Assets/Guns/bullet_shell.gd
+++ Assets/Guns/bullet_shell.gd
@@ -4,32 +4,45 @@ var travelled_distance: = 0
 var rng: = RandomNumberGenerator.new()
 var direction: Vector2
 var fase: = 1
-
-
+var final_rotation: float
+var falling_speed: Vector2
 var speed: = 30
+var falling: = true
 
 func _ready() -> void:
-	direction = Vector2(rng.randf_range(-10,5),rng.randf_range(-8,-15))
+	direction = Vector2(rng.randf_range(-5,5),rng.randf_range(-8,-16))
+	final_rotation = rng.randf_range(0.0,360.0)
 
 func _physics_process(delta: float) -> void:
-	
-	if fase == 1:
-		#direction.x = lerp(direction.x, 0.0, 0.2)
-		direction.y = lerp(direction.y, 0.0, 0.2)
-		if direction.y > -0.3:
-			z_index = 0
-			fase = 0
+	if falling:
+		if fase == 1:
+			#direction.x = lerp(direction.x, 0.0, 0.2)
+			direction.y = lerp(direction.y, 0.0, 0.2)
+			rotation_degrees = lerp(rotation_degrees, 180.0, 0.2)
+			if direction.y > -0.3:
+				z_index = 0
+				fase = 0
 
-	if fase == 0:
-		direction = Vector2(direction.x ,rng.randf_range(25,35))
-		fase = 2
-		
-	if fase == 2:
-		direction.x = lerp(direction.x, 0.0, 0.2)
-		direction.y = lerp(direction.y, 0.0, 0.2)
+		if fase == 0:
+			falling_speed = Vector2(direction.x ,rng.randf_range(30,70) * 10)
+			fase = 2
+			
+		if fase == 2:
+			#direction.x = lerp(direction.x, falling_speed.x, 0.2)
+			direction.y = lerp(direction.y, falling_speed.y, 0.03)
+			if direction.y > falling_speed.x -1:
+				fase = 3
+			
+		if fase == 3:
+			direction.x = lerp(direction.x, 0.0, 0.1)
+			direction.y = lerp(direction.y, 0.0, 0.1)
+			#if is_equal_approx(direction.y, 0.0):
+			if direction.y < 0.01:
+				falling = false
+			#rotation_degrees = lerp
 
-	print( str(direction.y))
-	position += direction * delta * speed
+		print( str(direction.y))
+		position += direction * delta * speed
 
 
 func ejecting() -> void:
