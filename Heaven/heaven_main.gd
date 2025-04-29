87dd988c219c0d82abe37241de2ba7942f12034a
DecotDev <decot013@gmail.com>
2025-04-29 17:02:09 +0200

    Hell music bug fixed
    What's fixed?
    - The line stopping the hell music player was on hell scene _ready instead than on its place, at heaven _ready. It has been moved and corrected.

--- Heaven/heaven_main.gd
+++ Heaven/heaven_main.gd
@@ -7,6 +7,7 @@ var heaven_gui: = %HeavenGUI
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	SoundPlayer.menu_music_player.stop()
+	SoundPlayer.background_music_player.stop()
 	Global.world = true
 
 func _on_portal_body_entered(body: Node2D) -> void:
